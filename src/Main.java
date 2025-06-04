993a4babbfee9127cd1fec1ce20bd3172ab24919
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-04 10:23:33 +0900

    second comparison operator

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
 
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a == b : " + (a != b));
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= 10 : " + (a >= 10));
+        System.out.println("a <= 5 : " + (a <= 5));
     }
 }
